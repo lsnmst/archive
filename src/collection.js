24642cb71148c3d3c16d5e0d965b5fc2a2069afe
lsnmst <musetta.alessandro@gmail.com>
2025-11-27 14:21:45 +0100

    Update ADD data.js END 60

--- src/collection.js
+++ src/collection.js
@@ -1,10 +1,14 @@
 const collection = {
     "Comunidados":
-        "Comunidados is the idea, the project, the collection of maps and experiences created with Giulia Tagliente, which inspired all the others in the archive.",
+        "Comunidados is an idea, the project, the collection of maps and experiences created with Giulia Tagliente, which inspired all the others in the archive.",
     "Mapping the Void":
         "Mapping the void is a collection of exercises for practising how to map what is usually termed void in mainstream cartography: thus, even a desert, a glacier or a forest fill the map with signs and meanings.",
     "Fisherwomen of the African Lakes":
         "Fisherwomen of the African Lakes collection brings together the work and collective actions implemented together with the women's fishing communities of lake Albert and lake Kivu.",
+    "Yoruba plants & cosmology":
+        "A collection exploring traditional knowledge of Yoruba plants, outside of Western epistemology and its hegemonic influence on ethnobotanical sciences.",
+    "Idjwi, the island of peace between enemy lines":
+        "The collection dedicated to the Idjwi Island, the sacred natural site on Lake Kivu that resists the armed conflict of the African Great Lakes."
 };
 
 export default collection;
